94d56df92bb366adb1b8ca8ae7386c6469bf79b8
congpham212 <congpham.bn@gmail.com>
2020-09-23 15:20:30 +0700

    Merge remote-tracking branch 'remotes/congpham212/branchofCuong' into reminder3
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/note_1/AddReminderActivity.java
+++ app/src/main/java/com/example/note_1/AddReminderActivity.java
@@ -9,7 +9,9 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -17,6 +19,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.NumberPicker;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
@@ -31,7 +34,10 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
     private Button btn_loop;
     private Button btn_everyDay;
 
+    EditText et;
 
+    ModeNotifPagerAdapter pgAdapter;
+    String s = "hahaha";
 
 
     @Override
@@ -44,16 +50,56 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
 
 
 
+
+
         anhXa();
 
         final TabLayout tlModeNotif = findViewById(R.id.tl_mode_notif);
         final ViewPager vpModeNotif = findViewById(R.id.vp_mode_notif);
-        ModeNotifPagerAdapter pgAdapter = new ModeNotifPagerAdapter(getSupportFragmentManager());
+        pgAdapter = new ModeNotifPagerAdapter(getSupportFragmentManager());
         vpModeNotif.setAdapter(pgAdapter);
         tlModeNotif.setupWithViewPager(vpModeNotif);
+        vpModeNotif.getCurrentItem();
+        vpModeNotif.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                if(i == 0){
+                    ModeNotifPagerAdapter.mode_notif_1 a = new ModeNotifPagerAdapter.mode_notif_1();
+
+                    Log.e("ZZZZZZZZZZZ: ", "0000000000000000000000");
+                }
+                if(i == 1){
+                    Log.e("ZZZZZZZZZZZ: ", "1111111111111111111111");
+                }
+                if(i == 2){
+                    ModeNotifPagerAdapter.mode_notif_3 mode3 = new ModeNotifPagerAdapter.mode_notif_3();
+                   mode3.anhXa();
+                    String s = mode3.getTime_repeat() == null ? "Null" : "Not NULL";
+                    Log.e("ZZZZZZZZZZZ: ", s);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+
+//        et.setText("10");
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.custom_actionbar_add_reminder, menu);
 
+        return super.onCreateOptionsMenu(menu);
+    }
 
     private void anhXa() {
         et_title = (EditText) findViewById(R.id.et_tile_main2);
@@ -65,8 +111,27 @@ public class AddReminderActivity extends AppCompatActivity implements View.OnCli
             case android.R.id.home:
                 finish();
                 return true;
+
+            case R.id.save_reminder:
+//                Toast.makeText(context, "Da luu", Toast.LENGTH_LONG).show();
+                save();
+//                finish();
+                return true;
         }
         return super .onOptionsItemSelected(item);
+    }
+
+    private void save() {
+        if(et == null){
+            Toast.makeText(context, "NULL", Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(context, s, Toast.LENGTH_LONG).show();
+        }
+
+//        s = String.valueOf(et.getText()) == null ? "NULL" : String.valueOf(et.getText());
+
+
     }
 
     @Override
--- app/src/main/java/com/example/note_1/ModeNotifPagerAdapter.java
+++ app/src/main/java/com/example/note_1/ModeNotifPagerAdapter.java
@@ -7,8 +7,13 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+
+import android.widget.NumberPicker;
+import android.widget.TimePicker;
 
 public class ModeNotifPagerAdapter extends FragmentPagerAdapter {
     private int numOfTab = 3;
@@ -51,12 +56,18 @@ public class ModeNotifPagerAdapter extends FragmentPagerAdapter {
         return null;
     }
     public static class mode_notif_1 extends Fragment {
+        View view;
+        TimePicker timePickerMode_1;
+        DatePicker datePickerMode_1;
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            view = inflater.inflate(R.layout.chon_gio, container, false);
+            timePickerMode_1 = view.findViewById(R.id.timePicker1);
+            datePickerMode_1 = view.findViewById(R.id.datePicker);
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.chon_gio, container, false);
+            return view;
         }
     }
 
@@ -65,23 +76,60 @@ public class ModeNotifPagerAdapter extends FragmentPagerAdapter {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
+            View view = inflater.inflate(R.layout.chon_gio_va_thu, container, false);
             // Inflate the layout for this fragment
-            return inflater.inflate(R.layout.chon_gio_va_thu, container, false);
+            return view;
         }
     }
     public static class mode_notif_3 extends Fragment {
+        View view;
+        public EditText time_repeat;
+        public NumberPicker time_type;
+
+        public EditText getTime_repeat() {
+            return time_repeat;
+        }
+
+        public void setTime_repeat(EditText time_repeat) {
+            this.time_repeat = time_repeat;
+        }
+
+        public NumberPicker getTime_type() {
+            return time_type;
+        }
+
+        public void setTime_type(NumberPicker time_type) {
+            this.time_type = time_type;
+        }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             // Inflate the layout for this fragment
-            View view = inflater.inflate(R.layout.chon_thoi_gian_lap_lai, container, false);
-            Spinner spLoopMode = (Spinner) view.findViewById(R.id.spLoopMode);
+            view = inflater.inflate(R.layout.chon_thoi_gian_lap_lai, container, false);
             String listLoopMode[] = {"Phút", "Giờ", "Ngày", "Tuần", "Tháng", "Năm"};
-            ArrayAdapter adapterSensorName = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, listLoopMode);
-            adapterSensorName.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
-            spLoopMode.setAdapter(adapterSensorName);
+            NumberPicker numberPicker = view.findViewById(R.id.number_Picker);
+            numberPicker.setValue(0);
+            numberPicker.setWrapSelectorWheel(false);
+            numberPicker.setMaxValue(listLoopMode.length-1);
+            numberPicker.setMinValue(0);
+            numberPicker.setDisplayedValues(listLoopMode);
+
+            anhXa();
+
+//            this.time_repeat = view.findViewById(R.id.et_thoi_gian_loop);
+//            this.time_type = view.findViewById(R.id.number_Picker);
+//            Spinner spLoopMode = (Spinner) view.findViewById(R.id.spLoopMode);
+
+//            ArrayAdapter adapterSensorName = new ArrayAdapter<String>(view.getContext(), android.R.layout.simple_spinner_dropdown_item, listLoopMode);
+//            adapterSensorName.setDropDownViewResource(android.R.layout.select_dialog_singlechoice);
+//            spLoopMode.setAdapter(adapterSensorName);
             return view;
+        }
+
+        public void anhXa() {
+            time_repeat = view.findViewById(R.id.et_thoi_gian_loop);
+            time_type = view.findViewById(R.id.number_Picker);
         }
     }
 }
